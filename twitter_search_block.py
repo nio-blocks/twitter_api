dd9206802a9147c5f0c1b0009ca5142b7fcf0071
Oren Leiman <oleiman@neutral.io>
2014-08-18 16:38:25 -0700

    Adds block structure
    Signed-off-by: Oren Leiman <oleiman@neutral.io>

---
+++ twitter_search_block.py
@@ -0,0 +1,9 @@
+import requests
+import json
+import oauth2 as oauth2
+from nio.common.block.base import Block
+from nio.common.discovery import Discoverable, DiscoverableType
+
+
+class TwitterAPIBlock(Block):
+    pass
